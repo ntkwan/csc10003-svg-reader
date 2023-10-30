97388c711d17bcd558f1ee39c6f9c8c6db1368e7
nkwn <npquan2704@gmail.com>
2023-10-30 15:38:21 +0700

    Add pugixml
    Update CMake and add sample .svg file

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-#include "raylib.h"
+#include "pugixml.hpp"
 
 int main() {
     constexpr int screen_width = 1600;
     constexpr int screen_height = 900;
 
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("sample/pic.svg");
+    if (!result) return -1;
+
+    auto get_attribute = [&](std::string name) {
+        auto node = doc.child("svg").child("circle");
+        auto attr = node.attribute(name.c_str());
+        return attr.value();
+    };
+
+    int pos_x = std::stoi(get_attribute("cx"));
+    int pos_y = std::stoi(get_attribute("cy"));
+    float radius = std::stof(get_attribute("r"));
+
     sf::RenderWindow window(sf::VideoMode(screen_width, screen_height),
                             "svg-reader-version-0.1");
-
-    constexpr int pos_x = 700;
-    constexpr int pos_y = 300;
-    constexpr float radius = 100.f;
     sf::CircleShape shape(radius, 1000);
     shape.setPosition(pos_x, pos_y);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -27,6 +38,5 @@ int main() {
         window.draw(shape);
         window.display();
     }
-
     return 0;
 }
