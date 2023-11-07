c5c63a8abed7fc001dd2968c29d7b9c61849bd8f
nkwn <21685516+ntkwan@users.noreply.github.com>
2023-11-07 11:59:21 +0700

    Add Line parser

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -28,7 +28,6 @@ std::string Parser::getAttribute(pugi::xml_node node, std::string name) {
 
 sf::Color Parser::parseColor(pugi::xml_node node, std::string name) {
     auto getAtribColor = [](std::string color) -> sf::Color {
-        int lst_pos = 4;
         std::string c = "";
         std::vector< int > rgb;
         for (int i = 4; i < (int)color.size(); ++i) {
@@ -43,7 +42,6 @@ sf::Color Parser::parseColor(pugi::xml_node node, std::string name) {
         }
 
         sf::Color result = sf::Color(rgb[0], rgb[1], rgb[2]);
-
         return result;
     };
 
@@ -74,17 +72,20 @@ void Parser::parseSVG() {
         float stroke_width = std::stof(getAttribute(tool, "stroke-width"));
 
         if (tool.name() == std::string("rect")) {
-            /*
-
-            RECTANGLE
-
-             */
+            Rect* shape = new Rect(std::stof(getAttribute(tool, "width")),
+                                   std::stof(getAttribute(tool, "height")),
+                                   std::stof(getAttribute(tool, "x")),
+                                   std::stof(getAttribute(tool, "y")),
+                                   fill_color, stroke_color, stroke_width);
+            shapes.push_back(shape);
         } else if (tool.name() == std::string("line")) {
-            /*
-
-            LINE
-
-            */
+            Line* shape =
+                new Line(sf::Vector2f(std::stof(getAttribute(tool, "x1")),
+                                      std::stof(getAttribute(tool, "y1"))),
+                         sf::Vector2f(std::stof(getAttribute(tool, "x2")),
+                                      std::stof(getAttribute(tool, "y2"))),
+                         stroke_color, stroke_width);
+            shapes.push_back(shape);
         } else if (tool.name() == std::string("text")) {
             /*
 
@@ -112,6 +113,7 @@ void Parser::parseSVG() {
             POLYGON
 
             */
+
         } else if (tool.name() == std::string("polyline")) {
             /*
 
