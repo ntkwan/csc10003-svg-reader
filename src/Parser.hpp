b5f0e53059b04200a7173dad1a28a3e5addc6744
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2023-11-14 16:52:47 +0700

    Add doxygen comments

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -5,87 +5,92 @@
 #include "pugixml.hpp"
 
 /**
- * @brief Represents a parser for parsing SVG files.
+ * @brief Represents a parser for SVG files.
  *
- * The Parser class is responsible for parsing SVG files and rendering the
- * shapes in the SVG file.
+ * The parser class is responsible for parsing XML attributes (SVG files) and
+ * passing this information to create shapes and render them.
  */
 class Parser {
 public:
     /**
-     * @brief Get the instance of the Parser.
-     *
-     * @param file_name The name of the file to be parsed.
-     *
-     * @return The instance of the Parser.
+     * @brief Gets the singleton instance of Parser.
+     * @param file_name The name of the SVG file to parse.
+     * @return The singleton instance of Parser.
+     * @note This function is thread-safe.
      */
     static Parser* getInstance(const std::string& file_name);
 
     /**
-     * @brief Deleted copy constructor.
-     *
-     * This function is deleted to prevent copying of the Parser.
-     *
-     * @param parser The Parser to be copied.
-     *
-     * @return The instance of the Parser.
-     *
+     * @brief Deleted assignment operator to prevent copying of Parser
+     * instances.
+     * @return The Parser instance.
+     * @note This function is deleted because Parser is a singleton class.
      */
     Parser(const Parser&) = delete;
 
     /**
-     * @brief Get attribute of the node.
-     *
-     * @param node The node to be parsed.
+     * @brief Get the Attribute object which is parsed from the XML file.
+     * @param node The node of the XML file (pugi::xml_node is a typedef of
+     * pugixml)
      * @param name The name of the attribute.
-     *
-     * @return The value of the attribute.
+     * @return The attribute which is parsed from the XML file.
+     * @note This function is private because it is only used by the Parser
+     * class.
      */
     std::string getAttribute(pugi::xml_node node, std::string name);
 
     /**
-     * @brief Parse color from the node.
-     *
-     * @param node The node to be parsed.
+     * @brief Parse the color from the XML file.
+     * @param node The node of the XML file (pugi::xml_node is a typedef of
+     * pugixml)
      * @param name The name of the attribute.
-     *
-     * @return The color of the attribute.
+     * @return The color which is parsed from the XML file.
+     * @note This function is private because it is only used by the Parser
+     * class.
+     * @note The color is represented by sf::Color (SFML)
+     * @note The color is parsed from the XML file in the format of
+     * "rgb(r, g, b)".
      */
     sf::Color parseColor(pugi::xml_node node, std::string name);
 
     /**
-     * @brief Parse points from the node.
-     *
-     * This function is used to parse points from the polygon and polyline
-     *
-     * @param node The node to be parsed.
-     *
-     * @return The points of the attribute.
+     * @brief Parse the points from the XML file.
+     * @param node The node of the XML file (pugi::xml_node is a typedef of
+     * pugixml)
+     * @return The points which are parsed from the XML file.
+     * @note This function is private because it is only used by the Parser
+     * class.
+     * @note The points are represented by std::vector< sf::Vector2f > (SFML)
+     * @note The points are parsed from the XML file in the format of
+     * "x1,y1 x2,y2 x3,y3 ...".
      */
     std::vector< sf::Vector2f > parsePoints(pugi::xml_node node);
 
     /**
-     * @brief Parse transform from the node.
-     *
-     * @param node The node to be parsed.
-     *
-     * @return The transform of the attribute.
+     * @brief Parse the XML file.
+     * @param window The window to render the shapes on.
+     * @note This function is private because it is only used by the Parser
+     * class.
+     * @note The shapes are rendered on the window by calling the draw() method
+     *  of the Shape class.
+     * @note The shapes are rendered on the window in the order of the XML file.
      */
     void parseSVG();
 
     /**
-     * @brief Render the SVG.
-     *
-     * This function is used to render the all the SVG shapes.
-     *
-     * @param window The window to be rendered.
+     * @brief Render the shapes on the window.
+     * @param window The window to render the shapes on.
+     * @note This function is private because it is only used by the Parser
+     * class.
+     * @note The shapes are rendered on the window by calling the draw() method
+     * of the Shape class.
+     * @note The shapes are rendered on the window in the order of the XML file.
+     * @note Apply Polymorphism to render the shapes on the window.
      */
     void renderSVG(sf::RenderWindow& window);
 
     /**
-     * @brief Destructor of the Parser.
-     *
-     * This function is used to delete all the shapes.
+     * @brief Destructor of Parser.
      */
     ~Parser();
 
--- src/graphics/Shape.hpp
+++ src/graphics/Shape.hpp
@@ -8,15 +8,16 @@
 #include <SFML/System/Vector2.hpp>
 
 /**
- * @brief Represents a drawable shape.
- *
- * The Shape class is an abstract class that represents a drawable shape.
+ * @brief Represents a shape in 2D space
+ * @note This class is abstract and cannot be instantiated.
+ * @note This class is applied Abstract Factory design pattern and used as
+ * interface for other shapes.
  */
 class Shape {
 public:
-    /**
-     * @brief Virtual destructor.
+    /*! @brief Virtual constructor
      */
+
     virtual ~Shape() = default;
 
     /**
@@ -34,153 +35,157 @@ public:
     void setOutlineColor(const sf::Color& color);
 
     /**
-     * @brief Sets the thickness of the shape's outline.
-     *
+     * @brief Sets the outline thickness of the shape.
      * @param thickness The new outline thickness of the shape.
+     * @note If the thickness is negative, the outline will be inside the shape.
+     * If the thickness is positive, the outline will be outside the shape.
+     * If the thickness is zero, no outline will be drawn.
+     * @note The default outline thickness is 0.
+     * @note The outline thickness cannot be greater than the radius of the
+     * shape.
      */
     void setOutlineThickness(float thickness);
 
     /**
-     * @brief Get fill color of the shape.
-     *
-     * @return Fill color of the shape.
+     * @brief Gets the fill color of the shape.
+     * @return The fill color of the shape.
+     * @note The default fill color is white.
      */
     const sf::Color& getFillColor() const;
 
     /**
-     * @brief Get outline color of the shape.
-     *
-     * @return Outline color of the shape.
+     * @brief Gets the outline color of the shape.
+     * @return The outline color of the shape.
+     * @note The default outline color is white.
      */
     const sf::Color& getOutlineColor() const;
 
     /**
-     * @brief Get outline thickness of the shape.
-     *
-     * @return Outline thickness of the shape.
+     * @brief Gets the outline thickness of the shape.
+     * @return The outline thickness of the shape.
+     * @note The default outline thickness is 0.
      */
     float getOutlineThickness() const;
 
     /**
-     * @brief Sets the local origin of the shape.
-     *
-     * The origin of an object defines the center point for all transformations
-     *
-     * @param x The X-coordinate of the origin.
-     * @param y The Y-coordinate of the origin.
+     * @brief Sets the position of the shape
+     * @param x The x coordinate of the new position
+     * @param y The y coordinate of the new position
+     * @note The default position of the shape is (0, 0).
+     * @note The position of the shape is relative to its origin.
      */
     void setPosition(float x, float y);
 
     /**
-     * @brief Sets the local origin of the shape.
-     *
-     * The origin of an object defines the center point for all transformations
-     *
-     * @param position The new origin of the shape.
+     * @brief Sets the position of the shape
+     * @param position The new position of the shape (sf::Vector2f is a typedef
+     * of coordination in SFML)
+     * @note The default position of the shape is (0, 0).
+     * @note The position of the shape is relative to its origin.
      */
     void setPosition(const sf::Vector2f& position);
 
     /**
-     * @brief Get the total number of points of the shape.
-     *
-     * @return Number of points of the shape.
+     * @brief Virtual method: Get the number of point of the shape (for Polygon
+     * shape)
+     * @return The number of points of the shape
+     * @note This is a pure virtual method, so it has to be implemented by the
+     * derived class to define how the shape should be drawn.
      */
     virtual std::size_t getPointCount() const = 0;
 
     /**
-     * @brief Get a point of the shape.
-     *
-     * The returned point is in local coordinates, that is, the shape's
+     * @brief Virtual method: Get the position of the point on the shape (for
+     * Polygon shape)
+     * @param index The index of the point
+     * @return The position of the specified point on the shape
+     * @note The returned point is in local coordinates, that is, the shape's
      * transforms (position, rotation, scale) are not taken into account.
-     * The result is undefined if index is out of the valid range.
-     *
-     * @param index Index of the point to get, in range [0 .. getPointCount() -
-     * 1]
-     *
-     * @return index-th point of the shape
+     * @note The result is undefined if index is out of the valid range.
+     * @note The number of points of the shape is defined by the concrete
+     * implementation.
+     * @note The returned vector is constant, which means that you can't modify
+     * its coordinates when you retrieve it.
+     * @note This is a pure virtual method, so it has to be implemented by the
+     * derived class.
      */
     virtual sf::Vector2f getPoint(std::size_t index) const = 0;
 
     /**
-     * @brief Draw the shape to a render target.
-     *
-     * @param target Render target to draw to
-     * @param states Current render states
+     * @brief Virtual method: Draw the shape on the specified render target
+     * @param target The render target (sf::Renderwindow is a typedef of SFML
+     * drawing window)
+     * @param states The render states to apply (default is
+     * sf::RenderStates::Default)
      */
     virtual void draw(
         sf::RenderWindow& target,
         sf::RenderStates states = sf::RenderStates::Default) const;
 
     /**
-     * @brief Get the combined transform of the object.
-     *
-     * @return Transform combining the position/rotation/scale/origin of the
-     * object
+     * @brief Gets the shape transform
+     * @return The shape transform (sf::Transform is a typedef of SFML)
+     * @note This function returns the combined transform of the object.
+     * @note The transform is a combination of the position, rotation, and scale
+     * of the object.
      */
     const sf::Transform& getTransform() const;
 
     /**
-     * @brief Get the inverse of the combined transform of the object.
-     *
-     * @return Inverse of the combined transformations of the object
+     * @brief Gets the inverse shape transform
+     * @return The inverse shape transform (sf::Transform is a typedef of SFML)
+     * @note This function returns the inverse of the combined transform of the
+     * object.
      */
     const sf::Transform& getInverseTransform() const;
 
 protected:
     /**
-     * @brief Default constructor.
+     * @brief Constructs a Shape object
+     * @note This constructor is protected because Shape is an abstract class
+     * that cannot be instantiated.
      */
     Shape();
 
     /**
-     * @brief Recompute the internal geometry of the shape.
-     *
-     * This function must be called by the derived class everytime
-     * the shape's points change (i.e. the result of either getPointCount
-     * or getPoint is different).
+     * @brief Sets the texture of the shape
+     * @param texture The new texture of the shape
+     * @note The texture is not copied, it is referenced by the shape.
+     * @note The default texture is NULL.
      */
     void update();
 
 private:
-    /**
-     * @brief Update the fill vertices' color.
-     */
     void updateFillColors();
 
-    /**
-     * @brief Update the outline vertices' position.
-     */
     void updateOutline();
 
-    /**
-     * @brief Update the outline vertices' color.
-     */
     void updateOutlineColors();
 
 private:
-    const sf::Texture* texture;  //!< Texture of the shape
-    sf::Color fill_color;        //!< Fill color
-    sf::Color outline_color;     //!< Outline color
-    float outline_thickness;     //!< Thickness of the shape's outline
-    sf::VertexArray vertices;    //!< Vertex array containing the fill geometry
-    sf::VertexArray outline_vertices;  //!< Vertex array containing the outline
-                                       //!< geometry
-    sf::FloatRect inside_bounds;  //!< Bounding rectangle of the inside (fill)
-    sf::FloatRect bounds;  //!< Bounding rectangle of the whole shape (outline
-                           //!< + fill)
+    const sf::Texture* texture;  ///< Texture of the shape
+    sf::Color fill_color;        ///< Fill color
+    sf::Color outline_color;     ///< Outline color
+    float outline_thickness;     ///< Thickness of the shape's outline
+    sf::VertexArray vertices;    ///< Vertex array containing the fill geometry
+    sf::VertexArray outline_vertices;  ///< Vertex array containing the outline
+                                       ///< geometry
+    sf::FloatRect inside_bounds;  ///< Bounding rectangle of the inside (fill)
+    sf::FloatRect bounds;  ///< Bounding rectangle of the whole shape (outline
+                           ///< + fill)
 
     sf::Vector2f
-        origin;  //!< Origin of translation/rotation/scaling of the object
-    sf::Vector2f position;  //!< Position of the object in the 2D world
-    float rotation;         //!< Orientation of the object, in degrees
-    sf::Vector2f scale;     //!< Scale of the object
-    mutable sf::Transform transform;  //!< Combined transformation of the object
+        origin;  ///< Origin of translation/rotation/scaling of the object
+    sf::Vector2f position;  ///< Position of the object in the 2D world
+    float rotation;         ///< Orientation of the object, in degrees
+    sf::Vector2f scale;     ///< Scale of the object
+    mutable sf::Transform transform;  ///< Combined transformation of the object
     mutable bool
-        transform_need_update;  //!< Does the transform need to be recomputed?
+        transform_need_update;  ///< Does the transform need to be recomputed?
     mutable sf::Transform
-        inverse_transform;  //!< Combined transformation of the object
-    mutable bool inverse_transform_need_update;  //!< Does the transform need to
-                                                 //!< be recomputed?
+        inverse_transform;  ///< Combined transformation of the object
+    mutable bool inverse_transform_need_update;  ///< Does the transform need to
+                                                 ///< be recomputed?
 };
 
 #endif  // SHAPE_HPP_
