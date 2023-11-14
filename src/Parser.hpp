f52678091afc74e6c22aafaa351400c74de21494
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2023-11-14 15:03:27 +0700

    Add doxygen comment for Parser and Shape

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -4,21 +4,101 @@
 #include "Graphics.hpp"
 #include "pugixml.hpp"
 
+/**
+ * @brief Represents a parser for parsing SVG files.
+ *
+ * The Parser class is responsible for parsing SVG files and rendering the
+ * shapes in the SVG file.
+ */
 class Parser {
 public:
+    /**
+     * @brief Get the instance of the Parser.
+     *
+     * @param file_name The name of the file to be parsed.
+     *
+     * @return The instance of the Parser.
+     */
     static Parser* getInstance(const std::string& file_name);
+
+    /**
+     * @brief Deleted copy constructor.
+     *
+     * This function is deleted to prevent copying of the Parser.
+     *
+     * @param parser The Parser to be copied.
+     *
+     * @return The instance of the Parser.
+     *
+     */
     Parser(const Parser&) = delete;
+
+    /**
+     * @brief Get attribute of the node.
+     *
+     * @param node The node to be parsed.
+     * @param name The name of the attribute.
+     *
+     * @return The value of the attribute.
+     */
     std::string getAttribute(pugi::xml_node node, std::string name);
+
+    /**
+     * @brief Parse color from the node.
+     *
+     * @param node The node to be parsed.
+     * @param name The name of the attribute.
+     *
+     * @return The color of the attribute.
+     */
     sf::Color parseColor(pugi::xml_node node, std::string name);
+
+    /**
+     * @brief Parse points from the node.
+     *
+     * This function is used to parse points from the polygon and polyline
+     *
+     * @param node The node to be parsed.
+     *
+     * @return The points of the attribute.
+     */
     std::vector< sf::Vector2f > parsePoints(pugi::xml_node node);
+
+    /**
+     * @brief Parse transform from the node.
+     *
+     * @param node The node to be parsed.
+     *
+     * @return The transform of the attribute.
+     */
     void parseSVG();
+
+    /**
+     * @brief Render the SVG.
+     *
+     * This function is used to render the all the SVG shapes.
+     *
+     * @param window The window to be rendered.
+     */
     void renderSVG(sf::RenderWindow& window);
+
+    /**
+     * @brief Destructor of the Parser.
+     *
+     * This function is used to delete all the shapes.
+     */
     ~Parser();
 
 private:
+    /**
+     * @brief Construct a new Parser object.
+     *
+     * @param file_name The name of the file to be parsed.
+     */
     Parser(const std::string& file_name);
-    static Parser* instance;
-    pugi::xml_node svg;
-    std::vector< Shape* > shapes;
+
+    static Parser* instance;       ///< The instance of the Parser.
+    pugi::xml_node svg;            ///< The node of the SVG.
+    std::vector< Shape* > shapes;  ///< The vector of the shapes.
 };
 #endif
--- src/graphics/Shape.hpp
+++ src/graphics/Shape.hpp
@@ -7,48 +7,154 @@
 #include <SFML/Graphics/VertexArray.hpp>
 #include <SFML/System/Vector2.hpp>
 
+/**
+ * @brief Represents a drawable shape.
+ *
+ * The Shape class is an abstract class that represents a drawable shape.
+ */
 class Shape {
 public:
+    /**
+     * @brief Virtual destructor.
+     */
     virtual ~Shape() = default;
 
+    /**
+     * @brief Sets the fill color of the shape.
+     *
+     * @param color The new fill color of the shape.
+     */
     void setFillColor(const sf::Color& color);
 
+    /**
+     * @brief Sets the outline color of the shape.
+     *
+     * @param color The new outline color of the shape.
+     */
     void setOutlineColor(const sf::Color& color);
 
+    /**
+     * @brief Sets the thickness of the shape's outline.
+     *
+     * @param thickness The new outline thickness of the shape.
+     */
     void setOutlineThickness(float thickness);
 
+    /**
+     * @brief Get fill color of the shape.
+     *
+     * @return Fill color of the shape.
+     */
     const sf::Color& getFillColor() const;
 
+    /**
+     * @brief Get outline color of the shape.
+     *
+     * @return Outline color of the shape.
+     */
     const sf::Color& getOutlineColor() const;
 
+    /**
+     * @brief Get outline thickness of the shape.
+     *
+     * @return Outline thickness of the shape.
+     */
     float getOutlineThickness() const;
 
+    /**
+     * @brief Sets the local origin of the shape.
+     *
+     * The origin of an object defines the center point for all transformations
+     *
+     * @param x The X-coordinate of the origin.
+     * @param y The Y-coordinate of the origin.
+     */
     void setPosition(float x, float y);
 
+    /**
+     * @brief Sets the local origin of the shape.
+     *
+     * The origin of an object defines the center point for all transformations
+     *
+     * @param position The new origin of the shape.
+     */
     void setPosition(const sf::Vector2f& position);
 
+    /**
+     * @brief Get the total number of points of the shape.
+     *
+     * @return Number of points of the shape.
+     */
     virtual std::size_t getPointCount() const = 0;
 
+    /**
+     * @brief Get a point of the shape.
+     *
+     * The returned point is in local coordinates, that is, the shape's
+     * transforms (position, rotation, scale) are not taken into account.
+     * The result is undefined if index is out of the valid range.
+     *
+     * @param index Index of the point to get, in range [0 .. getPointCount() -
+     * 1]
+     *
+     * @return index-th point of the shape
+     */
     virtual sf::Vector2f getPoint(std::size_t index) const = 0;
 
+    /**
+     * @brief Draw the shape to a render target.
+     *
+     * @param target Render target to draw to
+     * @param states Current render states
+     */
     virtual void draw(
         sf::RenderWindow& target,
         sf::RenderStates states = sf::RenderStates::Default) const;
 
+    /**
+     * @brief Get the combined transform of the object.
+     *
+     * @return Transform combining the position/rotation/scale/origin of the
+     * object
+     */
     const sf::Transform& getTransform() const;
 
+    /**
+     * @brief Get the inverse of the combined transform of the object.
+     *
+     * @return Inverse of the combined transformations of the object
+     */
     const sf::Transform& getInverseTransform() const;
 
 protected:
+    /**
+     * @brief Default constructor.
+     */
     Shape();
 
+    /**
+     * @brief Recompute the internal geometry of the shape.
+     *
+     * This function must be called by the derived class everytime
+     * the shape's points change (i.e. the result of either getPointCount
+     * or getPoint is different).
+     */
     void update();
 
 private:
+    /**
+     * @brief Update the fill vertices' color.
+     */
     void updateFillColors();
 
+    /**
+     * @brief Update the outline vertices' position.
+     */
     void updateOutline();
 
+    /**
+     * @brief Update the outline vertices' color.
+     */
     void updateOutlineColors();
 
 private:
